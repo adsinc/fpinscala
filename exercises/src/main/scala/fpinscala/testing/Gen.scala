d797ca24b5c9571fbdea1b817399a1120b9280d6
adsinc <sinc86@gmail.com>
2017-09-22 20:23:24 +0300

    chapter 7

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -30,7 +30,14 @@ object Prop {
 case class Gen[A](sample: State[RNG, A])
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] =
+    Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] =
+    Gen(State(RNG.nonNegativeInt).map(n => n % 2 == 0))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
 
   def choose(start: Int, stopExclusive: Int): Gen[Int] = {
     Gen(State(RNG.nonNegativeInt).map(i => start + i % (stopExclusive - start)))
