bc1a29099c63557738836702fa85d4b48159370f
adsinc <sinc86@gmail.com>
2017-09-25 20:30:34 +0300

    chapter 8

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -27,7 +27,16 @@ object Prop {
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
-case class Gen[A](sample: State[RNG, A])
+case class Gen[A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] =
+    Gen(sample.map(f))
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(x => f(x).sample))
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size.flatMap(s => Gen.listOfN(s, this))
+}
 
 object Gen {
   def unit[A](a: => A): Gen[A] =
@@ -42,12 +51,17 @@ object Gen {
   def choose(start: Int, stopExclusive: Int): Gen[Int] = {
     Gen(State(RNG.nonNegativeInt).map(i => start + i % (stopExclusive - start)))
   }
-}
 
-//trait Gen[A] {
-//  def map[A,B](f: A => B): Gen[B] = ???
-//  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-//}
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean flatMap(b => if(b) g1 else g2)
+
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val (gen1, w1) = g1
+    val (gen2, w2) = g2
+    val threshold = w1 / (w1 + w2)
+    Gen(State(RNG.double)) flatMap (w => if(w <= threshold) gen1 else gen2)
+  }
+}
 
 trait SGen[+A] {
 
--- exercises/src/main/scala/fpinscala/testing/test.sc
+++ exercises/src/main/scala/fpinscala/testing/test.sc
@@ -8,3 +8,7 @@ unit(10).sample.run(Simple(100))
 boolean.sample.run(Simple(10))
 
 listOfN(5, boolean).sample.run(Simple(101))
+
+val u = union(choose(1, 3), choose(10, 12))
+
+listOfN(10, u).sample.run(Simple(100))
