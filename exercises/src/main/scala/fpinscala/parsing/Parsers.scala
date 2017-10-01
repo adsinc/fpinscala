f2f417f7d14fa7689ecab23bc06fcf9dcf90e815
Aleksei Dolgii <sinc86@gmail.com>
2017-10-01 21:07:23 +0300

    parsing

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,15 +1,54 @@
 package fpinscala.parsing
 
-import language.higherKinds
+import fpinscala.testing._
 
-trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+import scala.language.higherKinds
+
+trait Parsers[ParseError, Parser[+ _]] {
+  self => // so inner classes may call methods of trait
+
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  def char(c: Char): Parser[Char] =
+    string(c.toString) map (_.head)
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  implicit def string(s: String): Parser[String]
+
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
+    ParserOps(f(a))
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+
+  def succeed[A](a: A): Parser[A] =
+    string("") map (_ => a)
 
   case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
+    def or[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
+    def many: Parser[List[A]] = self.many(p)
+
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      Prop.forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(identity))(in)
+
+    def succeedLaw[A](a: Parser[A])(in: Gen[String]): Prop =
+      Prop.forAll(in)(s => run(a)(s) == Right(a))
   }
 }
 
