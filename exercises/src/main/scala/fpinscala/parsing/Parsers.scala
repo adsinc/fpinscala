34d2de38f3f6ca82331a8dce5c2228360cfe58d5
Aleksei Dolgii <sinc86@gmail.com>
2017-10-02 21:29:38 +0300

    parsing

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -30,6 +30,18 @@ trait Parsers[ParseError, Parser[+ _]] {
   def succeed[A](a: A): Parser[A] =
     string("") map (_ => a)
 
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    (p map2 p.many) (_ :: _)
+
+  def product[A, B](p1: Parser[A], p2: Parser[B]): Parser[(A, B)]
+
+  def map2[A, B, C](a: Parser[A], b: Parser[B])(f: (A, B) => C): Parser[C] =
+    a ** b map { case (x, y) => f(x, y) }
+
+  char('a').many.slice.map(_.length) ** char('b').many1.slice.map(_.length)
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
@@ -38,6 +50,16 @@ trait Parsers[ParseError, Parser[+ _]] {
     def many: Parser[List[A]] = self.many(p)
 
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+
+    def slice: Parser[String] = self.slice(p)
+
+    def many1: Parser[List[A]] = self.many1(p)
+
+    def product[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+
+    def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+
+    def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
   }
 
   object Laws {
@@ -49,6 +71,10 @@ trait Parsers[ParseError, Parser[+ _]] {
 
     def succeedLaw[A](a: Parser[A])(in: Gen[String]): Prop =
       Prop.forAll(in)(s => run(a)(s) == Right(a))
+
+    def productLaw[A, B](a: Parser[A], b: Parser[B])(in: Gen[String]): Prop = {
+      Prop.forAll(in)(s => run(a ** b)(s) == Right((a, b)))
+    }
   }
 }
 
