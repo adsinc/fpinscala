f76f20331b5723c8d19f6d2f61852e5d07296d1d
Aleksei Dolgii <sinc86@gmail.com>
2017-10-03 22:34:13 +0300

    parsing

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -3,6 +3,7 @@ package fpinscala.parsing
 import fpinscala.testing._
 
 import scala.language.higherKinds
+import scala.util.matching.Regex
 
 trait Parsers[ParseError, Parser[+ _]] {
   self => // so inner classes may call methods of trait
@@ -31,7 +32,8 @@ trait Parsers[ParseError, Parser[+ _]] {
   def many[A](p: Parser[A]): Parser[List[A]] =
     (p map2 wrap(many(p))) (_ :: _) or succeed(List.empty)
 
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B] =
+    a flatMap (av => succeed(f(av)))
 
   def succeed[A](a: A): Parser[A] =
     string("") map (_ => a)
@@ -41,12 +43,26 @@ trait Parsers[ParseError, Parser[+ _]] {
   def many1[A](p: Parser[A]): Parser[List[A]] =
     (p map2 p.many) (_ :: _)
 
-  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)]
+  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    for {
+      a <- p1
+      b <- p2
+    } yield (a, b)
 
   def map2[A, B, C](a: Parser[A], b: => Parser[B])(f: (A, B) => C): Parser[C] =
     a ** b map f.tupled
 
-  char('a').many.slice.map(_.length) ** char('b').many1.slice.map(_.length)
+  def map2ViaFlatMap[A, B, C](a: Parser[A], b: => Parser[B])(f: (A, B) => C): Parser[C] =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  def regex[A](r: Regex): Parser[A]
+
+  val numCharParser = flatMap(regex("\\d+".r))(listOfN(_, char('a')))
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
@@ -66,6 +82,8 @@ trait Parsers[ParseError, Parser[+ _]] {
     def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
 
     def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
   }
 
   object Laws {
