2ea04d21668b74664bce42cb12cefa457b33c05e
adsinc <sinc86@gmail.com>
2017-11-29 23:02:13 +0300

    monads

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -148,9 +148,8 @@ case class Id[A](value: A) {
 object Reader {
   def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
     def unit[A](a: => A): Reader[R,A] = Reader(_ => a)
-    def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] = {
-      ???
-    }
+    def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] =
+      Reader(r => f(st.run(r)).run(r))
   }
 }
 
